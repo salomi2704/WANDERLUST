00ea9ac88eb941824255d12798603e53ef95bc93
salomi2704 <salomi2704@gmail.com>
2026-03-06 19:25:42 +0530

    Initial commit - Wanderlust project

---
+++ models/listing.js
@@ -0,0 +1,19 @@
+const mongoose=require("mongoose");
+const Schema=mongoose.Schema;
+
+const listingSchema=new Schema({
+    title:{
+        type:String,
+        required:true,
+    } ,
+    description: String,
+    image: {type:String,
+     set: (v)=> v==="" ? "https://unsplash.com/photos/a-single-cloud-floats-above-a-grassy-hill-SWmjxFob2SQ" :v,
+    },
+    price: Number,
+    location: String,
+    country: String,
+});
+
+const Listing =mongoose.model("Listing",listingSchema);
+modules.export=Listing;
