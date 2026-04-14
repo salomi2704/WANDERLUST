8527593b6bfbab68742e5c5c35e18615ac746a4c
salomi2704 <salomi2704@gmail.com>
2026-04-14 21:26:46 +0530

    Your message

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ const mongoose=require("mongoose");
 const Listing = require("../WANDERLUST/models/listing");
 const path=require("path");
 const MONGO_URL='mongodb://127.0.0.1:27017/wanderlust';
-
+const methodOverride= require("method-override");
+const ejsMate=require("ejs-mate");
 main()
 .then(()=>{
     console.log("connected to DB");
@@ -23,6 +24,9 @@ app.get("/",(req,res)=>{
 app.set("view engine","ejs");
 app.set("views",path.join(__dirname,"views")); 
 app.use(express.urlencoded({extended:true}));
+app.use(methodOverride("_method"));
+app.engine("ejs",ejsMate);
+app.use(express.static(path.join(__dirname,"/public")));
 // app.get("/testListing",async(req,res)=>{
 // let sampleListing =new Listing({
 //     title:"My New Villa",
@@ -40,13 +44,41 @@ app.get("/listings",async(req,res)=>{
     const allListings= await Listing.find({});
     res.render("listings/index",{allListings}); 
     });
-
-app.get("/listings/:id",async(req,res)=>{
-    let{id}=req.params;
-    const listing=await Listing.findById(id);
-    res.render("listings/show.ejs",{listing});
-
+app.get("/listings/new",(req,res)=>{
+    res.render("listings/new.ejs");
 });
+app.get("/listings/:id", async (req, res) => {
+  const { id } = req.params;
+  const listing = await Listing.findById(id);
+  if (!listing) {
+    return res.status(404).send("Listing not found");
+  }
+  res.render("listings/show", { listing });
+});
+
+
+app.post("/listings",async(req,res)=>{
+    const newListing=new Listing(req.body.listing);
+    await newListing.save();
+    res.redirect("/listings");
+});
+app.get("/listings/:id/edit",async(req,res)=>{
+    let {id}=req.params;
+    const listing =await Listing.findById(id);
+    res.render("listings/edit.ejs",{listing});
+});
+app.delete("/listings/:id",async(req,res)=>{
+    let {id}=req.params;
+    let deletedListing=await Listing.findByIdAndDelete(id);
+    console.log(deletedListing);
+    res.redirect("/listings")
+});
+app.put("/listings/:id",async(req,res)=>{
+    let{id}=req.params;
+   await Listing.findByIdAndUpdate(id,{...req.body.listing.image});
+   res.redirect(`/listings/${id}`);
+});
+
 app.listen(8080,()=>{
     console.log("server is listening to 8080");
 });
